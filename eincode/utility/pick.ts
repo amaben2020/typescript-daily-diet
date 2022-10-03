303d7cde90e6e4cae1e5421f44a2ad00829288c7
algomachine007 <amaben202phantom@gmail.com>
2022-10-03 18:32:33 +0100

    adding pick

--- eincode/utility/pick.ts
+++ eincode/utility/pick.ts
@@ -1,1 +1,26 @@
 // The TypeScript Pick type is a utility type which is used to create a new custom Type, based off an already existing one. It is the opposite of the Omit Type. Let's look at how it works.
+// https://hackernoon.com/how-the-typescript-pick-type-works
+
+type User = {
+  firstName: string,
+  lastName: string,
+  age: number
+}
+
+
+type UserName = Pick<User, "firstName" | "lastName"> // makes a new type which only has firstName and lastName:
+
+let customerInfo: UserName = {
+  firstName: "John",
+  lastName: "Doe"
+}
+
+
+
+type UserAge = Pick<User, "age">
+
+let customerAge: UserAge = {
+  age: 1534
+}
+
+// As you can see, the Pick type is very useful in creating custom types based on already existing ones. Now that you've mastered it, you can simplify your type declarations.
