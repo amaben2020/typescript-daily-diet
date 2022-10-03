5b0bc7de9f52aa4780044992e2f55001ff1603a5
algomachine007 <amaben202phantom@gmail.com>
2022-10-03 19:21:58 +0100

    building a generic function

---
+++ eincode/utility/generic.ts
@@ -0,0 +1,17 @@
+function pickObjectKeys<T, K extends keyof T>(obj: T, keys: K[]) {
+  let result = {} as Pick<T, K>
+  for (const key of keys) {
+    if (key in obj) {
+      result[key] = obj[key]
+    }
+  }
+  return result
+}
+
+const languageInfo = {
+  name: "TypeScript",
+  age: 8,
+  extensions: ['ts', 'tsx']
+}
+
+const ageAndExtensions = pickObjectKeys(languageInfo, ['age', 'extensions'])
--- eincode/utility/pick.ts
+++ eincode/utility/pick.ts
@@ -61,10 +61,3 @@ const getHighestKill = (users: TUser[]): string => {
 console.log(getHighestKill(users))
 
 
-function firstElement<Type>(arr: Type[]): Type {
-  return arr[0];
-}
-// s is of type 'string'
-const s = firstElement(["a", "b", "c"]);
-// n is of type 'number'
-const n = firstElement([1, 2, 3]);
