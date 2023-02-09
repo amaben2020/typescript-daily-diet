dea8a5b684e678f80a0c893e83f5c25f4f1f9ebf
algomachine007 <amaben202phantom@gmail.com>
2023-02-09 09:37:17 +0100

    using return type for function as type

--- totalTypescript/awaited.ts
+++ totalTypescript/awaited.ts
@@ -12,7 +12,7 @@ const URL = "https://jsonplaceholder.typicode.com/todos/1";
 const fetchData = async (): Promise<TUser | undefined> => {
   try {
     const { data }: Awaited<{ data: TUser }> = await axios.get(URL);
-    console.log("DATA", data);
+
     return data;
   } catch (error) {
     console.log(error);
@@ -21,3 +21,73 @@ const fetchData = async (): Promise<TUser | undefined> => {
 
 // const info: Awaited<ReturnType<typeof fetchData>> = fetchData();
 console.log(fetchData());
+
+const myInfo = {
+  name: "Ben",
+};
+
+type Images<S, N> = {
+  node: {
+    originalSrc: S;
+    altText: S;
+    width: N;
+    height: N;
+  };
+}[];
+
+const images = [
+  {
+    node: {
+      originalSrc: "Men-Jacket-Front-Black__15466.1603283963.png",
+      altText: "",
+      width: 2048,
+      height: 2048,
+    },
+  },
+  {
+    node: {
+      originalSrc: "Men-Jacket-Side-Black__68202.1603283961.png",
+      altText: "",
+      width: 2048,
+      height: 2048,
+    },
+  },
+  {
+    node: {
+      originalSrc: "Men-Jacket-Back-Black__33864.1603283963.png",
+      altText: "",
+      width: 2048,
+      height: 2048,
+    },
+  },
+  {
+    node: {
+      originalSrc: "Men-Jacket-Front-White__95300.1603283963.png",
+      altText: "",
+      width: 2048,
+      height: 2048,
+    },
+  },
+  {
+    node: {
+      originalSrc: "Men-Jacket-Side-White__91924.1603283962.png",
+      altText: "",
+      width: 2048,
+      height: 2048,
+    },
+  },
+  {
+    node: {
+      originalSrc: "Men-Jacket-Back-White__02866.1603283963.png",
+      altText: "",
+      width: 2048,
+      height: 2048,
+    },
+  },
+] satisfies Images<string, number>;
+
+const normalizer = (array: Images<string, number>): boolean | string[] => {
+  return [...array].map((elem) => elem.node["originalSrc"]);
+};
+
+console.log(normalizer(images));
