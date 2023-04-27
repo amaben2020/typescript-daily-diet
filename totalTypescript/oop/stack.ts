bbee6beceb6523e48140735614a5894514fea608
algomachine007 <amaben202phantom@gmail.com>
2023-04-27 09:01:40 +0100

    building a stack

--- totalTypescript/oop/stack.ts
+++ totalTypescript/oop/stack.ts
@@ -28,27 +28,57 @@ class Stack1 {
     return deleteItem;
   }
 
-  //Views first item
-  peek() {}
+  //Views first item i.e last element added to call stack
+  peek() {
+    console.log("peek", this.items[this.count - 1]);
+    return this.items[this.count - 1];
+  }
 
   //Returns boolean if there's an item in stack
-  isEmpty() {}
+  isEmpty() {
+    if (this.count == 0) {
+      return "Stack is empty";
+    } else {
+      return "Stack is not empty";
+    }
+  }
 
   // Returns number of items in call stack
-  size() {}
+  size() {
+    return this.count;
+  }
 
   // Print elements in stack
 
-  print() {}
+  print() {
+    let str = "";
+    for (let i = 0; i < this.count; i++) {
+      console.log("count", this.count);
+      str += this.items[i] + " ";
+    }
+
+    return str;
+  }
 
   // deletes all items from stack
-  clear() {}
+  clear() {
+    this.count = 0;
+    this.items = [];
+    console.log("Stack cleared");
+    return this.items;
+  }
 }
 
 const stack1 = new Stack1();
 
 stack1.push(300);
 stack1.push(400);
-console.log(stack1.push(400));
+// console.log(stack1.push(400));
 
-stack1.pop();
+// stack1.pop();
+// stack1.peek();
+
+console.log(stack1.isEmpty());
+console.log(stack1.size());
+console.log(stack1.print());
+console.log(stack1.clear());
