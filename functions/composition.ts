68c1a437c60ac52d97571fb8e7db5efbf3384156
algomachine007 <amaben202phantom@gmail.com>
2023-06-18 10:21:00 +0100

    typing the object

--- functions/composition.ts
+++ functions/composition.ts
@@ -1,16 +1,16 @@
 type TF1 = (a: number) => boolean;
 type TF2 = (a: TUserInDB) => number;
+
 type TUserInDB = {
   name: string;
   age: number;
 };
 
+const composed = (f: TF1, g: TF2) => (x: TUserInDB) => f(g(x));
 const getAge = (user: TUserInDB): number => user.age;
 const isAllowedAge = (age: number) => age > 20;
 
 const userInDb: TUserInDB = { name: "Adam", age: 25 };
-
-const composed = (f: TF1, g: TF2) => (x: TUserInDB) => f(g(x));
 
 const isAllowedToDrink = composed(isAllowedAge, getAge)(userInDb);
 
